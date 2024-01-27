1248f87d6679d457e7d37fdbe8d8c2e03e279a53
SuicideG4M3R <suicideg4mer@gmail.com>
2024-01-27 21:52:15 +0100

    Reworking

--- players/views.py
+++ players/views.py
@@ -1,7 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 
 
 class HomeView(View):
     def get(self, request):
-        return render(request, 'index.html')
+        return render(request, 'home.html')
+
+
+class ListPlayersView(View):
+    def get(self, request):
+        return HttpResponse('ListPlayers')

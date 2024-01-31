50c493407196a63c1c934b4b1481bd4d7f00c388
SuicideG4M3R <suicideg4mer@gmail.com>
2024-01-31 22:49:13 +0100

    Added object counter in list_view

--- logged/views.py
+++ logged/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth import logout, authenticate, login
+from django.contrib import messages
+from django.db import IntegrityError
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views import View
 
-from logged.forms import LoginForm
+from logged.forms import *
 
 
 class LoginView(View):
@@ -28,3 +30,57 @@ class LogoutView(View):
     def get(self, request):
         logout(request)
         return redirect('home')
+
+
+# class AddGamesView(View):
+#     def get(self, request):
+#         form = AddGamesForm()
+#         return render(request, 'add_form.html', {'form': form})
+
+
+class AddPlayersView(View):
+    def get(self, request):
+        form = AddPlayersForm()
+        return render(request, 'add_form.html', {'form': form})
+
+
+class AddClansView(View):
+    pass
+
+
+class AddTanksView(View):
+    def get(self, request):
+        form = AddTanksForm()
+        return render(request, 'add_form.html', {'form': form})
+
+    def post(self, request):
+        form = AddTanksForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data.get('name')
+            tier = form.cleaned_data.get('tier')
+            nation = form.cleaned_data.get('nation')
+            type = form.cleaned_data.get('type')
+            messages.success(request, f'Added tank: {name} to database')
+            return redirect('home')
+        return render(request, 'add_form.html', {'form': form})
+
+
+class AddTeamsView(View):
+    def get(self, request):
+        form = AddTeamsForm()
+        return render(request, 'add_form.html', {'form': form})
+
+    def post(self, request):
+        form = AddTeamsForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data.get('name')
+            player1 = form.cleaned_data.get('player1')
+            player2 = form.cleaned_data.get('player2')
+            player3 = form.cleaned_data.get('player3')
+            player4 = form.cleaned_data.get('player4')
+            player5 = form.cleaned_data.get('player5')
+            team = Team.objects.create(name=name)
+            team.players.add(player1, player2, player3, player4, player5)
+            team.save()
+            return redirect('home')
+        return render(request, 'add_form.html', {'form': form})

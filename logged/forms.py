482c66d9085b76a9d307ec5de04331564a67aec0
SuicideG4M3R <suicideg4mer@gmail.com>
2024-02-01 19:33:14 +0100

    Add views + register

--- logged/forms.py
+++ logged/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User, Group
 from django.core.exceptions import ValidationError
+from django.forms import Select
+
 from players.models import *
 from django.db.models import Q
 
@@ -11,14 +13,64 @@ class LoginForm(forms.Form):
     password = forms.CharField(max_length=50, widget=forms.PasswordInput)
 
 
+class RegisterForm(forms.ModelForm):
+    form_name = 'Register'
+    password = forms.CharField(max_length=50, min_length=8,
+                               widget=forms.PasswordInput(attrs={'class': 'form-control'}), label='Password')
+
+    password2 = forms.CharField(max_length=50, min_length=8,
+                                widget=forms.PasswordInput(attrs={'class': 'form-control'}), label='Re-Password')
+
+    class Meta:
+        model = User
+        fields = ['username',]
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+        }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password2 = cleaned_data.get('password2')
+        if password is None or password2 is None or password != password2:
+            raise ValidationError('Passwords do not match.')
+        if len(password) < 8 or 8 > len(password2):
+            raise ValidationError('Password must be at least 8 characters long.')
+        return cleaned_data
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(username=username).exists():
+            raise ValidationError('Username already exists.')
+        return username
+
+
 class AddGamesForm(forms.Form):
-    pass
+    form_name = 'Add New Game'
+    teams = Team.objects.all()
+    team1 = forms.ModelChoiceField(queryset=teams, required=True, label='Team 1')
+    team2 = forms.ModelChoiceField(queryset=teams, required=True, label='Team 2')
+    winner = forms.ChoiceField(choices=[(1, 'Team 1'), (2, 'Team 2')], required=True, label='Won')
+
+    def clean(self):
+        cleaned_data = super().clean()
+        team1 = cleaned_data.get('team1')
+        team2 = cleaned_data.get('team2')
+        if team1 == team2:
+            raise ValidationError('Teams cannot be the same.')
+        return cleaned_data
 
 
 class AddPlayersForm(forms.Form):
     form_name = 'Add New Player'
     nickname = forms.CharField(max_length=255)
     clan = forms.ModelChoiceField(queryset=Clan.objects.all(), required=False)
+
+    def clean_nickname(self):
+        nickname = self.cleaned_data.get('nickname')
+        if Player.objects.filter(nickname=nickname).exists():
+            raise ValidationError('Player with this nickname already exists')
+        return nickname
 
 
 class AddTanksForm(forms.Form):
@@ -34,6 +86,12 @@ class AddTanksForm(forms.Form):
             raise ValidationError('Tank with this name already exists')
         return name
 
+    def clean_tier(self):
+        tier = self.cleaned_data.get('tier')
+        if tier > 10 or tier < 1:
+            raise ValidationError('Tier must be between 1 and 10')
+        return tier
+
 
 def check_if_same_player_in_team(player1, player2, player3, player4, player5):
     players = [player1, player2, player3, player4, player5]
@@ -44,11 +102,12 @@ def check_if_same_player_in_team(player1, player2, player3, player4, player5):
 class AddTeamsForm(forms.Form):
     form_name = 'Add New Team'
     name = forms.CharField(max_length=255)
-    player1 = forms.ModelChoiceField(queryset=Player.objects.all(), label='Player 1')
-    player2 = forms.ModelChoiceField(queryset=Player.objects.all(), label='Player 2')
-    player3 = forms.ModelChoiceField(queryset=Player.objects.all(), label='Player 3')
-    player4 = forms.ModelChoiceField(queryset=Player.objects.all(), label='Player 4')
-    player5 = forms.ModelChoiceField(queryset=Player.objects.all(), label='Player 5')
+    players = Player.objects.all()
+    player1 = forms.ModelChoiceField(queryset=players, label='Player 1')
+    player2 = forms.ModelChoiceField(queryset=players, label='Player 2')
+    player3 = forms.ModelChoiceField(queryset=players, label='Player 3')
+    player4 = forms.ModelChoiceField(queryset=players, label='Player 4')
+    player5 = forms.ModelChoiceField(queryset=players, label='Player 5')
 
     team_name_duplicate_error = 'Team with this name already exists'
 
@@ -69,3 +128,17 @@ class AddTeamsForm(forms.Form):
         check_if_same_player_in_team(player1, player2, player3, player4, player5)
 
         return cleaned_data
+
+
+class AddClanForm(forms.Form):
+    form_name = 'Add New Clan'
+    name = forms.CharField(max_length=50, min_length=3, required=True)
+    description = forms.CharField(max_length=255, required=False)
+
+    def clean_name(self):
+        name = self.cleaned_data.get('name')
+        if len(name) < 3:
+            raise ValidationError('Clan name must be at least 3 characters long')
+        if Clan.objects.filter(name=name).exists():
+            raise ValidationError('Clan with this name already exists')
+        return name
--- logged/views.py
+++ logged/views.py
@@ -1,5 +1,9 @@
+import random
+from datetime import datetime
+
 from django.contrib.auth import logout, authenticate, login
 from django.contrib import messages
+from django.contrib.auth.mixins import PermissionRequiredMixin, UserPassesTestMixin
 from django.db import IntegrityError
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
@@ -29,26 +33,95 @@ class LoginView(View):
 class LogoutView(View):
     def get(self, request):
         logout(request)
+        messages.success(request, 'Successfully logged out')
         return redirect('home')
 
 
-# class AddGamesView(View):
-#     def get(self, request):
-#         form = AddGamesForm()
-#         return render(request, 'add_form.html', {'form': form})
+class RegisterView(View):
+    def get(self, request):
+        form = RegisterForm()
+        return render(request, 'add_form.html', {'form': form})
+
+    def post(self, request):
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            if User.objects.all().count() == 0:
+                user = User.objects.create_superuser(username=username, password=password)
+                login(request, user)
+                messages.success(request, f'Successfully created SUPER user: {username}')
+            else:
+                user = User.objects.create_user(username=username, password=password)
+                login(request, user)
+                messages.success(request, f'Successfully created user: {username}')
+            return redirect('home')
+        return render(request, 'add_form.html', {'form': form})
 
 
-class AddPlayersView(View):
+class AddGamesView(PermissionRequiredMixin, View):
+    permission_required = ['players.add_game']
+
+    def get(self, request):
+        form = AddGamesForm()
+        return render(request, 'add_form.html', {'form': form})
+
+    def post(self, request):
+        form = AddGamesForm(request.POST)
+        if form.is_valid():
+            team1 = form.cleaned_data['team1']
+            team2 = form.cleaned_data['team2']
+            winner = int(form.cleaned_data['winner'])
+            if winner == 1:
+                winner = team1
+            else:
+                winner = team2
+            game = Game.objects.create(team1=team1, team2=team2)
+            GameResult.objects.create(winner_team=winner, game=game)
+            messages.success(request, f'Added game between team: {team1.name} vs team: {team2.name} --> Winner! {winner.name}')
+            return redirect('home')
+        return render(request, 'add_form.html', {'form': form})
+
+
+class AddPlayersView(PermissionRequiredMixin, View):
+    permission_required = ['players.add_player']
+
     def get(self, request):
         form = AddPlayersForm()
         return render(request, 'add_form.html', {'form': form})
 
+    def post(self, request):
+        form = AddPlayersForm(request.POST)
+        if form.is_valid():
+            nickname = form.cleaned_data.get('nickname')
+            clan = form.cleaned_data.get('clan')
+            Player.objects.create(nickname=nickname, clan=clan)
+            messages.success(request, f'Added player: {nickname} to database')
+            return redirect('home')
+        return render(request, 'add_form.html', {'form': form})
 
-class AddClansView(View):
-    pass
+
+class AddClansView(PermissionRequiredMixin, View):
+    permission_required = ['players.add_clans']
+
+    def get(self, request):
+        form = AddClanForm()
+        return render(request, 'add_form.html', {'form': form})
+
+    def post(self, request):
+        form = AddClanForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data.get('name')
+            description = form.cleaned_data.get('description')
+            Clan.objects.create(name=name, description=description)
+            messages.success(request, f'Successfully added {name}')
+            return redirect('home')
+        return render(request, 'add_form.html', {'form': form})
 
 
-class AddTanksView(View):
+class AddTanksView(PermissionRequiredMixin, View):
+    permission_required = ['players.add_tank']
+
     def get(self, request):
         form = AddTanksForm()
         return render(request, 'add_form.html', {'form': form})
@@ -60,12 +133,15 @@ class AddTanksView(View):
             tier = form.cleaned_data.get('tier')
             nation = form.cleaned_data.get('nation')
             type = form.cleaned_data.get('type')
+            Tank.objects.create(name=name, tier=tier, nation=nation, type=type)
             messages.success(request, f'Added tank: {name} to database')
             return redirect('home')
         return render(request, 'add_form.html', {'form': form})
 
 
-class AddTeamsView(View):
+class AddTeamsView(PermissionRequiredMixin, View):
+    permission_required = ['players.add_team']
+
     def get(self, request):
         form = AddTeamsForm()
         return render(request, 'add_form.html', {'form': form})
@@ -82,5 +158,67 @@ class AddTeamsView(View):
             team = Team.objects.create(name=name)
             team.players.add(player1, player2, player3, player4, player5)
             team.save()
+            messages.success(request, f'Added team: {name}: {player1} {player2} {player3} {player4} {player5}')
             return redirect('home')
         return render(request, 'add_form.html', {'form': form})
+
+
+class AddBasicDataView(UserPassesTestMixin, View):
+
+    def test_func(self):
+        return self.request.user.is_superuser
+
+    def handle_no_permission(self):
+        messages.error(self.request, f'Only admins can generate basic data')
+        return redirect('home')
+
+    def get(self, request, amount):
+        counter = 1
+        cycle = 0
+        while amount > cycle:
+            if Clan.objects.filter(name=f'Basic Clan {counter}').exists():
+                counter += 1
+            else:
+                try:
+                    clan = Clan.objects.create(name=f'Basic Clan {counter}',
+                                               description=f'Description of Basic Clan {counter}')
+                    player1 = Player.objects.create(nickname=f'Basic PlayerA {counter}', clan_id=clan.id)
+                    player2 = Player.objects.create(nickname=f'Basic PlayerB {counter}', clan_id=clan.id)
+                    player3 = Player.objects.create(nickname=f'Basic PlayerC {counter}', clan_id=clan.id)
+                    player4 = Player.objects.create(nickname=f'Basic PlayerD {counter}', clan_id=clan.id)
+                    player5 = Player.objects.create(nickname=f'Basic PlayerE {counter}', clan_id=clan.id)
+                    player11 = Player.objects.create(nickname=f'Basic PlayerF {counter}', clan_id=clan.id)
+                    player12 = Player.objects.create(nickname=f'Basic PlayerG {counter}', clan_id=clan.id)
+                    player13 = Player.objects.create(nickname=f'Basic PlayerH {counter}', clan_id=clan.id)
+                    player14 = Player.objects.create(nickname=f'Basic PlayerI {counter}', clan_id=clan.id)
+                    player15 = Player.objects.create(nickname=f'Basic PlayerJ {counter}', clan_id=clan.id)
+                    team1 = Team.objects.create(name=f'Basic TeamA {counter}')
+                    team2 = Team.objects.create(name=f'Basic TeamB {counter}')
+                    tank_type = TankType.objects.create(name=f'Basic Tank Type {counter}')
+                    nation = Nation.objects.create(name=f'Basic Nation {counter}')
+                    tank = Tank.objects.create(name=f'Basic Tank {counter}', nation_id=nation.id,
+                                               type_id=tank_type.id, tier=random.randint(1, 10))
+
+                    players = [player1, player2, player3, player4, player5, player11, player12, player13, player14,
+                               player15]
+                    for player in players:
+                        PlayerTank.objects.create(player_id=player.id, tank_id=tank.id)
+
+                    team1.players.add(player1, player2, player3, player4, player5)
+                    team2.players.add(player11, player12, player13, player14, player15)
+
+                    game = Game.objects.create(date_played=datetime.now(),
+                                               team1_id=team1.id, team2_id=team2.id)
+                    GameResult.objects.create(game_id=game.id, winner_team_id=team1.id)
+                    cycle += 1
+                    counter += 1
+                    print(f'Successfully created data for cycle {cycle}')
+                except Exception as error:
+                    print(f'Error creating data for cycle {cycle}')
+                    print(f'Error creating data for counter {counter}')
+                    print(f'Error: {error}')
+                    return HttpResponse(
+                        f'Error: {error} has occurred when creating data for cycle: {cycle} with counter: {counter}')
+
+        messages.success(request, f'Added basic data in amount of {amount} units.')
+        return redirect('home')

b89126af0e1dad93e0b0d856a09c589846d48390
SuicideG4M3R <suicideg4mer@gmail.com>
2024-02-07 17:29:13 +0100

    tests for detail, edit and delete

--- players/tests.py
+++ players/tests.py
@@ -1,10 +1,5 @@
 import pytest
-from django.contrib.contenttypes.models import ContentType
-from django.http import HttpResponseRedirect
-from django.test import TestCase
 from django.test import Client
-from django.urls import reverse
-
 from logged.forms import *
 from players.forms import *
 
@@ -455,6 +450,374 @@ def test_add_team_view_post(my_user_with_permissions, players):
 
 
 # ##################################### DETAIL VIEWS #####################################
+
+@pytest.mark.django_db
+def test_game_detail_view_get(game_result):
+    client = Client()
+    url = reverse('game_detail', args=(game_result.id,))
+    response = client.get(url)
+    assert response.status_code == 200
+    assert 'context' in response.context
+    assert game_result.winner_team in response.context['context'].values()
+    assert game_result.id in response.context['context'].values()
+
+
+@pytest.mark.django_db
+def test_game_detail_view_get_non_existing():
+    client = Client()
+    url = reverse('game_detail', args=(1,))
+    response = client.get(url)
+    assert response.status_code == 302
+    assert response.url == reverse('home')
+
+
+@pytest.mark.django_db
+def test_player_detail_view_get(player):
+    client = Client()
+    url = reverse('player_detail', args=(player.id,))
+    response = client.get(url)
+    assert response.status_code == 200
+    assert 'context' in response.context
+    assert player.nickname in response.context['context'].values()
+    assert player.clan in response.context['context'].values()
+
+
+@pytest.mark.django_db
+def test_player_detail_view_get_non_existing():
+    client = Client()
+    url = reverse('player_detail', args=(1,))
+    response = client.get(url)
+    assert response.status_code == 302
+    assert response.url == reverse('home')
+
+
+@pytest.mark.django_db
+def test_tank_detail_view_get(tank):
+    client = Client()
+    url = reverse('tank_detail', args=(tank.id,))
+    response = client.get(url)
+    assert response.status_code == 200
+    assert 'context' in response.context
+    assert tank.name in response.context['context'].values()
+    assert tank.tier in response.context['context'].values()
+    assert tank.type in response.context['context'].values()
+    assert tank.nation in response.context['context'].values()
+
+
+@pytest.mark.django_db
+def test_tank_detail_view_get_non_existing():
+    client = Client()
+    url = reverse('tank_detail', args=(1,))
+    response = client.get(url)
+    assert response.status_code == 302
+    assert response.url == reverse('home')
+
+
+@pytest.mark.django_db
+def test_clan_detail_view_get(clan):
+    client = Client()
+    url = reverse('clan_detail', args=(clan.id,))
+    response = client.get(url)
+    assert response.status_code == 200
+    assert 'context' in response.context
+    assert clan.name in response.context['context'].values()
+    assert clan.description in response.context['context'].values()
+
+
+@pytest.mark.django_db
+def test_clan_detail_view_get_non_existing():
+    client = Client()
+    url = reverse('clan_detail', args=(1,))
+    response = client.get(url)
+    assert response.status_code == 302
+    assert response.url == reverse('home')
+
+
+@pytest.mark.django_db
+def test_team_detail_view_get(team):
+    client = Client()
+    url = reverse('team_detail', args=(team.id,))
+    response = client.get(url)
+    assert response.status_code == 200
+    assert team.name in response.context['context'].values()
+    assert team.id in response.context['context'].values()
+
+
+@pytest.mark.django_db
+def test_team_detail_view_get_non_existing():
+    client = Client()
+    url = reverse('team_detail', args=(1,))
+    response = client.get(url)
+    assert response.status_code == 302
+    assert response.url == reverse('home')
+
+
+# ##################################### EDIT VIEWS #####################################
+
+@pytest.mark.django_db
+def test_game_edit_view_get(my_superuser, game_result):
+    client = Client()
+    client.force_login(my_superuser)
+    url = reverse('edit_game', args=(game_result.id,))
+    response = client.get(url)
+    assert response.status_code == 200
+    assert 'form' in response.context
+    assert isinstance(response.context['form'], AddGamesForm)
+
+
+@pytest.mark.django_db
+def test_game_edit_view_post(my_superuser, game_result):
+    client = Client()
+    client.force_login(my_superuser)
+    url = reverse('edit_game', args=(game_result.id,))
+    before_winner = game_result.winner_team_id
+    data = {
+        'team1': game_result.game.team1_id,
+        'team2': game_result.game.team2_id,
+        'winner': 2
+    }
+    response = client.post(url, data)
+    assert response.status_code == 302
+    updated_winner = GameResult.objects.get(id=game_result.id)
+    assert updated_winner.winner_team_id != str(before_winner)
+    assert updated_winner.winner_team_id == 2
+
+
+@pytest.mark.django_db
+def test_player_edit_view_get(my_user_with_permissions, player):
+    client = Client()
+    client.force_login(my_user_with_permissions)
+    url = reverse('edit_player', args=(player.id,))
+    response = client.get(url)
+    assert response.status_code == 200
+    assert 'form' in response.context
+    assert isinstance(response.context['form'], AddPlayersForm)
+
+
+@pytest.mark.django_db
+def test_player_edit_view_post(my_user_with_permissions, player):
+    client = Client()
+    client.force_login(my_user_with_permissions)
+    url = reverse('edit_player', args=(player.id,))
+    form_data = {
+        'nickname': 'Updated Nickname',
+        'clan': player.clan_id
+    }
+    assert not player.nickname == 'Updated Nickname'
+    response = client.post(url, form_data)
+    assert response.status_code == 302
+    player = Player.objects.get(id=player.id)
+    assert player.nickname == 'Updated Nickname'
+
+
+@pytest.mark.django_db
+def test_tank_edit_view_get(my_user_with_permissions, tank):
+    client = Client()
+    client.force_login(my_user_with_permissions)
+    url = reverse('edit_tank', args=(tank.id,))
+    response = client.get(url)
+    assert response.status_code == 200
+    assert 'form' in response.context
+    assert isinstance(response.context['form'], AddTanksForm)
+
+
+@pytest.mark.django_db
+def test_tank_edit_view_post(my_user_with_permissions, tank):
+    client = Client()
+    client.force_login(my_user_with_permissions)
+    url = reverse('edit_tank', args=(tank.id,))
+    form_data = {
+        'name': 'Updated Tank Name',
+        'tier': 9,
+        'nation': tank.nation_id,
+        'type': tank.type_id
+    }
+    assert not tank.name == 'Updated Tank Name'
+    assert tank.tier == 8
+    response = client.post(url, form_data)
+    assert response.status_code == 302
+    tank = Tank.objects.get(id=tank.id)
+    assert tank.name == 'Updated Tank Name'
+    assert tank.tier == 9
+
+
+@pytest.mark.django_db
+def test_clan_edit_view_get(my_user_with_permissions, clan):
+    client = Client()
+    client.force_login(my_user_with_permissions)
+    url = reverse('edit_clan', args=(clan.id,))
+    response = client.get(url)
+    assert response.status_code == 200
+    assert 'form' in response.context
+    assert isinstance(response.context['form'], AddClanForm)
+
+
+@pytest.mark.django_db
+def test_clan_edit_view_post(my_user_with_permissions, clan):
+    client = Client()
+    client.force_login(my_user_with_permissions)
+    url = reverse('edit_clan', args=(clan.id,))
+    form_data = {
+        'name': 'Updated Clan Name',
+        'description': 'Updated description'
+    }
+    assert not clan.name == 'Updated Clan Name'
+    assert not clan.description == 'Updated description'
+    response = client.post(url, form_data)
+    assert response.status_code == 302
+    clan = Clan.objects.get(id=clan.id)
+    assert clan.name == 'Updated Clan Name'
+    assert clan.description == 'Updated description'
+
+
+@pytest.mark.django_db
+def test_team_edit_view_get(my_user_with_permissions, team):
+    client = Client()
+    client.force_login(my_user_with_permissions)
+    url = reverse('edit_team', args=(team.id,))
+    response = client.get(url)
+    assert response.status_code == 200
+    assert 'form' in response.context
+    assert isinstance(response.context['form'], AddTeamsForm)
+
+
+@pytest.mark.django_db
+def test_team_edit_view_post(my_user_with_permissions, team):
+    client = Client()
+    client.force_login(my_user_with_permissions)
+    url = reverse('edit_team', args=(team.id,))
+    form_data = {
+        'name': 'Updated Team Name',
+        'player1': team.players.all()[0].id,
+        'player2': team.players.all()[1].id,
+        'player3': team.players.all()[2].id,
+        'player4': team.players.all()[3].id,
+        'player5': team.players.all()[4].id
+    }
+    assert not team.name == 'Updated Team Name'
+    response = client.post(url, form_data)
+    assert response.status_code == 302
+    team = Team.objects.get(id=team.id)
+    assert team.name == 'Updated Team Name'
+
+
+# ##################################### DELETE SUPERUSER VIEWS #####################################
+
+@pytest.mark.django_db
+def test_games_delete_view_with_superuser_permissions(my_superuser, games):
+    client = Client()
+    client.force_login(my_superuser)
+    assert Game.objects.all().count() == 7
+    url = reverse('delete_game', args=(games[0].id,))
+    response = client.get(url)
+    assert response.status_code == 302
+    assert response.url == reverse('games_list')
+    assert Game.objects.all().count() == 6
+
+
+@pytest.mark.django_db
+def test_game_delete_view_with_no_permissions(my_user, games):
+    client = Client()
+    client.force_login(my_user)
+    assert Game.objects.all().count() == 7
+    url = reverse('delete_game', args=(games[0].id,))
+    response = client.get(url)
+    assert response.status_code == 302
+    assert response.url == reverse('games_list')
+    assert not Game.objects.all().count() == 6
+
+
+@pytest.mark.django_db
+def test_players_delete_view_with_permissions(my_user_with_permissions, players):
+    client = Client()
+    client.force_login(my_user_with_permissions)
+    assert Player.objects.all().count() == 7
+    url = reverse('delete_player', args=(players[0].id,))
+    response = client.get(url)
+    assert response.status_code == 302
+    assert response.url == reverse('players_list')
+    assert Player.objects.all().count() == 6
+
+
+@pytest.mark.django_db
+def test_player_delete_view_with_no_permissions(my_user, players):
+    client = Client()
+    client.force_login(my_user)
+    assert Player.objects.all().count() == 7
+    url = reverse('delete_player', args=(players[0].id,))
+    response = client.get(url)
+    assert response.status_code == 403
+    assert not Player.objects.all().count() == 6
+
+
+@pytest.mark.django_db
+def test_tank_delete_view_with_permissions(my_user_with_permissions, tanks):
+    client = Client()
+    client.force_login(my_user_with_permissions)
+    assert Tank.objects.all().count() == 7
+    url = reverse('delete_tank', args=(tanks[0].id,))
+    response = client.get(url)
+    assert response.status_code == 302
+    assert response.url == reverse('tanks_list')
+    assert Tank.objects.all().count() == 6
+
+
+@pytest.mark.django_db
+def test_tank_delete_view_with_no_permissions(my_user, tanks):
+    client = Client()
+    client.force_login(my_user)
+    assert Tank.objects.all().count() == 7
+    url = reverse('delete_tank', args=(tanks[0].id,))
+    response = client.get(url)
+    assert response.status_code == 403
+    assert not Tank.objects.all().count() == 6
+
+
+@pytest.mark.django_db
+def test_clan_delete_view_with_permissions(my_user_with_permissions, clans):
+    client = Client()
+    client.force_login(my_user_with_permissions)
+    assert Clan.objects.all().count() == 7
+    url = reverse('delete_clan', args=(clans[0].id,))
+    response = client.get(url)
+    assert response.status_code == 302
+    assert response.url == reverse('clans_list')
+    assert Clan.objects.all().count() == 6
+
+
+@pytest.mark.django_db
+def test_clan_delete_view_with_no_permissions(my_user, clans):
+    client = Client()
+    client.force_login(my_user)
+    assert Clan.objects.all().count() == 7
+    url = reverse('delete_clan', args=(clans[0].id,))
+    response = client.get(url)
+    assert response.status_code == 403
+    assert not Clan.objects.all().count() == 6
+
+
+@pytest.mark.django_db
+def test_team_delete_view_with_permissions(my_user_with_permissions, teams):
+    client = Client()
+    client.force_login(my_user_with_permissions)
+    assert Team.objects.all().count() == 7
+    url = reverse('delete_team', args=(teams[0].id,))
+    response = client.get(url)
+    assert response.status_code == 302
+    assert response.url == reverse('teams_list')
+    assert Team.objects.all().count() == 6
+
+
+@pytest.mark.django_db
+def test_team_delete_view_with_no_permissions(my_user, teams):
+    client = Client()
+    client.force_login(my_user)
+    assert Team.objects.all().count() == 7
+    url = reverse('delete_team', args=(teams[0].id,))
+    response = client.get(url)
+    assert response.status_code == 403
+    assert not Team.objects.all().count() == 6
+
 
 # ##################################### SPECIAL SUPERUSER VIEWS #####################################
 
